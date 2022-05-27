54fd463120deb9867d6060dd488029a35e1df153
Vanessa Oliveira <vanessa.cs.oliveira@gmail.com>
2022-05-26 22:22:08 -0300

    changing image landing

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiArrowRight, FiSearch, FiMap, FiMapPin } from 'react-icons/fi'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowRight, FiSearch, FiMap, FiMapPin } from "react-icons/fi";
 
-import './styles.css'
-import logoImg from '../../images/logoeco.svg'
+import "./styles.css";
+import logoImg from "../../images/logoeco.svg";
 
 // import { Container } from './styles';
 
 const Landing: React.FC = () => {
   return (
     <div id="page-landing">
-    <div className="content-wrapper">
-      <img src={logoImg} alt="Re-Eduque"/>
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Re-Eduque" />
 
-      <main>
-        <h1>Leve felicidade para o mundo</h1>
-        <p>Visite pontos de coleta e descarte e mude o meio ambiente.</p>
-      </main>
+        <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite pontos de coleta e descarte e mude o meio ambiente.</p>
+        </main>
 
-      <div className="location">
-        <strong>Recife</strong>
-        <span>Pernambuco</span>
+        <div className="location">
+          <strong>Recife</strong>
+          <span>Pernambuco</span>
+        </div>
+
+        <Link to="app" className="enter-app">
+          <FiMap size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
+
+        <Link to="news" className="enter-news">
+          <FiSearch size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
       </div>
-
-      <Link to="app" className="enter-app">
-        <FiMap size={26} color="rgba(0, 0, 0, 0.6)" />
-      </Link>
-
-      <Link to="news" className="enter-news">
-        <FiSearch size={26} color="rgba(0, 0, 0, 0.6)" />
-      </Link>
     </div>
-   </div>
-  )
-}
+  );
+};
 
 export default Landing;
